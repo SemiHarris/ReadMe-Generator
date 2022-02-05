72e4f9f32e11c4aeec6620dbdaaace549a0f7717
Semi <Semiharris3@gmail.com>
2022-02-05 07:05:53 -0500

    Finihed the question and now working on displaying in the readMe

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?(Required)',
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('Please enter a title for the project');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is the username for the github repository?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the app!'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install the app?'
+    },
+    {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'What is the license for the app?',
+        choices: ['MIT','Apache','GPL']
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use the app?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can people contribute to the app?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test the app?'
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('../README.md', fileName, err => {
+            
+        if (err) {
+            reject(err);
+            
+            return;
+        }
+
+        resolve({
+            ok: true,
+            message: 'File created!'
+            });
+        });
+    });
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
-init();
+init()
+
