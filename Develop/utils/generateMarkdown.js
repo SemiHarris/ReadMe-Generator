b56e9213d8b5ef8cfa9518890b68181e32ebc80b
Semi <Semiharris3@gmail.com>
2022-02-06 01:19:41 -0500

    Finished the license Section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,15 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseObj = license.map(license => {
+    const licens = {
+      Name: license,
+      Badge: `https://img.shields.io/badge/License-${license}-yellow.svg`,
+    }
+    return licens
+  })
+  return renderLicenseLink(licenseObj)
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const link = license.map(license => {
+     license.Link = (`https://opensource.org/licenses/${license.Name}`)
+     return license
+  })
+  return link
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  const displaySection = renderLicenseBadge(license).map(section => {
+    return `
+  ${section.Name} License  
+  ![License: ${section.Name}]${section.Badge}  
+  ${section.Link}  `
+  })
+  return displaySection
+};
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
@@ -17,4 +39,7 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+//module.exports = generateMarkdown;
+console.log((renderLicenseSection(['Apache','Semi','Leaf','Sand','Rock','Sound','Cloud','Mist'])))
+
+
